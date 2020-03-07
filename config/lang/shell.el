622cf0de30f11dc97f44ae51dd461e0c88a31051
Dmitry <shshkn@tuta.io>
2020-03-07 19:24:08 +0300

    shell: unbind C-c C-c/C-f

--- config/lang/shell.el
+++ config/lang/shell.el
@@ -1,3 +1,6 @@
+(define-key sh-mode-map (kbd "C-c C-c") nil)
+(define-key sh-mode-map (kbd "C-c C-f") nil)
+
 (use-package flymake-shellcheck
   :hook ((sh-mode .  flymake-mode)
          (sh-mode .  flymake-shellcheck-load)))
