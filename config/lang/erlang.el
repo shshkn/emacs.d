fb93b4aef7491d9b3d73b8788586a9e6b67f0b6c
Dmitry <shshkn@tuta.io>
2020-05-01 23:43:56 +0300

    erlang: align shell's buffer

--- config/lang/erlang.el
+++ config/lang/erlang.el
@@ -3,4 +3,6 @@
   :init
   (setq lsp-erlang-server-path (expand-file-name
                                 "erlang_ls/bin/erlang_ls"
-                                ds-dir-data-root)))
+                                ds-dir-data-root))
+  :config
+  (add-to-list 'shackle-rules '("\*erlang\*.*" :regexp t :select t :align below)))
